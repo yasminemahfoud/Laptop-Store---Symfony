9dd30a29975c4bd28c6d7afb1e5929f5dffe0570
Hassan <hassanassaid402@gmail.com>
2026-06-15 09:31:03 +0100

    Add user orders page with login protection

--- src/Controller/OrderController.php
+++ src/Controller/OrderController.php
@@ -7,6 +7,7 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 final class OrderController extends AbstractController
 {
@@ -20,6 +21,17 @@ final class OrderController extends AbstractController
     {
         return $this->render('order/index.html.twig', [
             'controller_name' => 'OrderController',
+        ]);
+    }
+
+    #[Route('/user/orders', name: 'app_user_orders')]
+    #[IsGranted('ROLE_USER')]
+    public function userOrders(): Response
+    {
+        $user = $this->getUser();
+
+        return $this->render('order/user_orders.html.twig', [
+            'orders' => $user->getOrders(),
         ]);
     }
 }
